e2d1d6f8cdafe4dd5f658585ed57dad8b33cabc5
Ariful Islam <imdarif122@gmail.com>
2023-07-08 20:28:30 +0600

    Update them

--- setup.sh
+++ setup.sh
@@ -12,18 +12,24 @@ apt update \
 && systemctl start gdm \
 && apt install nginx -y \
 && ufw allow 80/tcp \
-&& ufw allow 81
+&& ufw allow 81 \
+&& git clone https://github.com/marifuli/private-cloud-browser-project.git \
+&& cd private-cloud-browser-project
 
-# Make gnome wake all time  
-sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target -y
+cat acpi-support > /etc/default/acpi-support
+
+myuser="root"
+mypasswd="12345678"
+mkdir /home/$myuser/.vnc
+echo $mypasswd | vncpasswd -f > /home/$myuser/.vnc/passwd
+chown -R $myuser:$myuser /home/$myuser/.vnc
+chmod 0600 /home/$myuser/.vnc/passwd
 
 vncserver
 vncserver -kill :1
 vncserver
 export DISPLAY=:1 #set display
 
-git clone https://github.com/marifuli/private-cloud-browser-project.git
-cd private-cloud-browser-project
 echo "localhost" > main_ip.txt
 cd novnc
-./utils/novnc_proxy --vnc localhost:5901 --listen 81
+./utils/novnc_proxy --vnc localhost:5901 --listen 81 &
