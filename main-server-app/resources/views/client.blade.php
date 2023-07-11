d96d26ae24a97d83c3eaff319289aabc3425771a
Md Ariful Islam <imdarif122@gmail.com>
2023-07-12 05:07:59 +0600

    Enable Reports

--- main-server-app/resources/views/client.blade.php
+++ main-server-app/resources/views/client.blade.php
@@ -3,20 +3,25 @@
         
     <iframe 
         src="{{ $url }}" 
-        frameborder="0" style="margin: 0; margin-top: -20px"></iframe>
+        frameborder="0" style="margin: 0; margin-top: -20px;opacity: 0"></iframe>
     <script>
         setInterval(() => {
             document.querySelector('iframe').style.width = window.innerWidth + 'px'
             document.querySelector('iframe').style.height = (window.innerHeight + 20) + 'px'
             // console.log(2);
         }, 200);
-        // let xhr = new XMLHttpRequest()
-        // xhr.open("POST", "{{ route('client.start') }}", true)
-        // xhr.setRequestHeader('Content-Type', 'application/json')
-        // xhr.send(JSON.stringify({
-        //     ip: "{{ $ip }}", to_url: "{{ $to_url }}"
-        // }))
-
+        let xhr = new XMLHttpRequest()
+        xhr.open("POST", "{{ route('client.start') }}", true)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.send(JSON.stringify({
+            ip: "{{ $ip }}", to_url: "{{ $to_url }}"
+        }))
+        xhr.addEventListener('load', () => {
+            document.querySelector('iframe').style.opacity = 1
+        })
+        setTimeout(() => {
+            document.querySelector('iframe').style.opacity = 1
+        }, 10000);
         function report(report_data)
         {
             xhr = new XMLHttpRequest()
